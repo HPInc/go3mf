3ffd9290a4b349bcee5f62327d9bb9e882dd1611
Quim Muntal Diaz <quim.muntal.diaz@hp.com>
2018-10-17 16:36:52 +0200

    add types

---
+++ internal/common/types.go
@@ -0,0 +1,21 @@
+package common
+
+type Char int8
+type WChar rune
+type Byte byte
+type Uint16 uint16
+type Int16 int16
+type Int32 int32
+type Error int32
+type Bool bool
+type Float float32
+type Double float64
+type Uint32 uint32
+type Color uint32
+type Int64 int64
+type Uint64 uint64
+type TimeStamp uint64
+
+type Float64Pair struct {
+	a, b float64
+}
