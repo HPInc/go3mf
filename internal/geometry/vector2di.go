3088dcd71a8efa38393ea428a49506bf2f1c10d2
Quim Muntal Diaz <quim.muntal.diaz@hp.com>
2018-10-18 16:52:08 +0200

    add docs to vectors

--- internal/geometry/vector2di.go
+++ internal/geometry/vector2di.go
@@ -16,6 +16,7 @@ func NewVector2DI(x, y int32) Vector2DI {
 	return Vector2DI{x, y}
 }
 
+//Cast returns the Vector2D version of the vector.
 func (a Vector2DI) Uncast(units float32) (Vector2D, error) {
 	if units < VectorMinUnits || units > VectorMaxUnits {
 		return Vector2D{}, common.NewError(common.ErrorInvalidUnits)
@@ -23,26 +24,32 @@ func (a Vector2DI) Uncast(units float32) (Vector2D, error) {
 	return NewVector2D(float32(a.X)*units, float32(a.Y)*units), nil
 }
 
+// Add returns a new vector that is the sum of both vectors.
 func (a Vector2DI) Add(b Vector2DI) Vector2DI {
 	return Vector2DI{a.X + b.X, a.Y + b.Y}
 }
 
+// Sub returns a new vector that is the substraction of both vectors.
 func (a Vector2DI) Sub(b Vector2DI) Vector2DI {
 	return Vector2DI{a.X - b.X, a.Y - b.Y}
 }
 
+// Scale returns a the vector scaled by the given factor.
 func (a Vector2DI) Scale(b int32) Vector2DI {
 	return Vector2DI{a.X * b, a.Y * b}
 }
 
+// Dot returns the dot product of both vectors.
 func (a Vector2DI) Dot(b Vector2DI) int64 {
 	return int64(a.X*b.X + a.Y*b.Y)
 }
 
+// Length returns the length of the vector.
 func (a Vector2DI) Length() float32 {
 	return float32(math.Sqrt(float64(a.X*a.X + a.Y*a.Y)))
 }
 
+// Distance returns the distance between both vectors.
 func (a Vector2DI) Distance(b Vector2DI) float32 {
 	return a.Sub(b).Length()
 }
