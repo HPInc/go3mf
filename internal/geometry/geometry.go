67466f862273bd278180ceab02a192e8dc883c28
Quim Muntal <quimmuntal@gmail.com>
2018-10-20 17:17:36 +0200

    improve pairmatch doc

--- internal/geometry/geometry.go
+++ internal/geometry/geometry.go
@@ -3,9 +3,11 @@ package geometry
 // PairMatch defines an interface which is able to identify duplicate pairs of numbers in a given data set.
 type PairMatch interface {
 	// AddMatch adds a match to the set.
+	// If the match exists it is overriden.
 	AddMatch(data1, data2, param int32)
 	// CheckMatch check if a match is in the set.
 	CheckMatch(data1, data2 int32) (val int32, ok bool)
 	// DeleteMatch deletes a match from the set.
+	// If match doesn't exist it bevavhe as a no-op
 	DeleteMatch(data1, data2 int32)
 }
--- internal/geometry/treepairmatch.go
+++ internal/geometry/treepairmatch.go
@@ -10,19 +10,25 @@ type TreePairMatch struct {
 	entries map[pairEntry]int32
 }
 
+// NewTreePairMatch creates a new TreePairMatch
 func NewTreePairMatch() *TreePairMatch {
 	return &TreePairMatch{map[pairEntry]int32{}}
 }
 
+// AddMatch adds a match to the set.
+// If the match exists it is overriden.
 func (t *TreePairMatch) AddMatch(data1, data2, param int32) {
 	t.entries[newPairEntry(data1, data2)] = param
 }
 
+// CheckMatch check if a match is in the set.
 func (t *TreePairMatch) CheckMatch(data1, data2 int32) (val int32, ok bool) {
 	val, ok = t.entries[newPairEntry(data1, data2)]
 	return
 }
 
+// DeleteMatch deletes a match from the set.
+// If match doesn't exist it bevavhe as a no-op
 func (t *TreePairMatch) DeleteMatch(data1, data2 int32) {
 	delete(t.entries, newPairEntry(data1, data2))
 }
