9cc169c7b4b8c5bf9b8a67b436b6d669d812b6f0
qmuntal <quimmuntal@gmail.com>
2018-11-04 19:57:34 +0100

    add docs to errors

--- internal/mesh/errors.go
+++ internal/mesh/errors.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 )
 
+// DuplicatedNodeError happens when an Edge with two identical nodes has been tried to add to a mesh.
 type DuplicatedNodeError struct{}
 
 func (e *DuplicatedNodeError) Error() string {
 	return "an Edge with two identical nodes has been tried to add to a mesh"
 }
 
+// MaxFaceError happens when a Face has been tried to add to a mesh with too many faces.
 type MaxFaceError struct {
 }
 
@@ -17,6 +19,7 @@ func (e *MaxFaceError) Error() string {
 	return fmt.Sprintf("a Face has been tried to add to a mesh with too many faces (%d)", MaxFaceCount)
 }
 
+// MaxNodeError happens when a Node has been tried to add to a mesh with too many nodes.
 type MaxNodeError struct {
 }
 
@@ -24,6 +27,7 @@ func (e *MaxNodeError) Error() string {
 	return fmt.Sprintf("a Node has been tried to add to a mesh with too many nodes (%d)", MaxNodeCount)
 }
 
+// MaxBeamError happens when a Beam has been tried to add to a mesh with too many beams.
 type MaxBeamError struct {
 }
 
