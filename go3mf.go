267cabd71162edee0d12d9f955f3205e64da3f42
qmuntal <quimmuntal@gmail.com>
2019-04-05 18:10:38 +0200

    remove unused default structs

--- go3mf.go
+++ go3mf.go
@@ -28,9 +28,9 @@ type Object interface {
 // Metadata item is an in memory representation of the 3MF metadata,
 // and can be attached to any 3MF model node.
 type Metadata struct {
-	Name  string
-	Value string
-	Type string
+	Name     string
+	Value    string
+	Type     string
 	Preserve bool
 }
 
@@ -45,23 +45,6 @@ type Attachment struct {
 type ProductionAttachment struct {
 	RelationshipType string
 	Path             string
-}
-
-// DefaultBaseMaterial defines the default base material property.
-type DefaultBaseMaterial struct {
-	ResourceID    uint64
-	ResourceIndex uint64
-}
-
-// DefaultColor defines the default color property.
-type DefaultColor struct {
-	Color color.RGBA
-}
-
-// DefaultTexCoord2D defines the default textture coordinates property.
-type DefaultTexCoord2D struct {
-	ResourceID uint64
-	U, V       float32
 }
 
 // BeamLatticeAttributes defines the Model Mesh BeamLattice Attributes class and is part of the BeamLattice extension to 3MF.
