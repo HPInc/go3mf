e3cf574ef4f769de612dc0c5f70a5479fe12055a
qmuntal <quimmuntal@gmail.com>
2020-02-18 08:07:32 +0100

    beamlattice encoding

--- beamlattice/encoder.go
+++ beamlattice/encoder.go
@@ -1,7 +1,95 @@
 package beamlattice
 
-import "github.com/qmuntal/go3mf"
+import (
+	"encoding/xml"
+	"strconv"
+
+	"github.com/qmuntal/go3mf"
+)
 
 func (m *BeamLattice) Marshal3MF(x *go3mf.XMLEncoder) error {
+	xs := xml.StartElement{Name: xml.Name{Space: ExtensionName, Local: attrBeamLattice}, Attr: []xml.Attr{
+		{Name: xml.Name{Local: attrMinLength}, Value: strconv.FormatFloat(float64(m.MinLength), 'f', x.FloatPresicion(), 10)},
+		{Name: xml.Name{Local: attrRadius}, Value: strconv.FormatFloat(float64(m.DefaultRadius), 'f', x.FloatPresicion(), 10)},
+	}}
+	if m.ClipMode != ClipNone {
+		xs.Attr = append(xs.Attr, xml.Attr{Name: xml.Name{Local: attrClippingMode}, Value: m.ClipMode.String()})
+	}
+	if m.ClippingMeshID != 0 {
+		xs.Attr = append(xs.Attr, xml.Attr{
+			Name:  xml.Name{Local: attrClippingMesh},
+			Value: strconv.FormatUint(uint64(m.ClippingMeshID), 10)},
+		)
+	}
+	if m.RepresentationMeshID != 0 {
+		xs.Attr = append(xs.Attr, xml.Attr{
+			Name:  xml.Name{Local: attrRepresentationMesh},
+			Value: strconv.FormatUint(uint64(m.RepresentationMeshID), 10)},
+		)
+	}
+	if m.CapMode != CapModeSphere {
+		xs.Attr = append(xs.Attr, xml.Attr{Name: xml.Name{Local: attrCap}, Value: m.CapMode.String()})
+	}
+	x.EncodeToken(xs)
+
+	marshalBeams(x, m)
+	marshalBeamsets(x, m)
+
+	x.EncodeToken(xs.End())
 	return nil
+}
+
+func marshalBeamsets(x *go3mf.XMLEncoder, m *BeamLattice) {
+	xb := xml.StartElement{Name: xml.Name{Space: ExtensionName, Local: attrBeamSets}}
+	x.EncodeToken(xb)
+	for _, bs := range m.BeamSets {
+		xbs := xml.StartElement{Name: xml.Name{Space: ExtensionName, Local: attrBeamSet}}
+		if bs.Name != "" {
+			xbs.Attr = append(xbs.Attr, xml.Attr{Name: xml.Name{Local: attrName}, Value: bs.Name})
+		}
+		if bs.Identifier != "" {
+			xbs.Attr = append(xbs.Attr, xml.Attr{Name: xml.Name{Local: attrIdentifier}, Value: bs.Identifier})
+		}
+		x.SetAutoClose(true)
+		for _, ref := range bs.Refs {
+			x.EncodeToken(xml.StartElement{Name: xml.Name{Space: ExtensionName, Local: attrRef}, Attr: []xml.Attr{
+				{Name: xml.Name{Local: attrIndex}, Value: strconv.FormatUint(uint64(ref), 10)},
+			}})
+		}
+		x.SetAutoClose(true)
+	}
+	x.EncodeToken(xb.End())
+}
+
+func marshalBeams(x *go3mf.XMLEncoder, m *BeamLattice) {
+	xb := xml.StartElement{Name: xml.Name{Space: ExtensionName, Local: attrBeams}}
+	x.EncodeToken(xb)
+	x.SetAutoClose(true)
+	for _, b := range m.Beams {
+		xbeam := xml.StartElement{Name: xml.Name{Space: ExtensionName, Local: attrBeam}, Attr: []xml.Attr{
+			{Name: xml.Name{Local: attrV1}, Value: strconv.FormatUint(uint64(b.NodeIndices[0]), 10)},
+			{Name: xml.Name{Local: attrV2}, Value: strconv.FormatUint(uint64(b.NodeIndices[1]), 10)},
+		}}
+		if b.Radius[0] > 0 {
+			xbeam.Attr = append(xbeam.Attr, xml.Attr{
+				Name:  xml.Name{Local: attrR1},
+				Value: strconv.FormatFloat(float64(b.Radius[0]), 'f', x.FloatPresicion(), 10),
+			})
+		}
+		if b.Radius[1] > 0 {
+			xbeam.Attr = append(xbeam.Attr, xml.Attr{
+				Name:  xml.Name{Local: attrR2},
+				Value: strconv.FormatFloat(float64(b.Radius[1]), 'f', x.FloatPresicion(), 10),
+			})
+		}
+		if b.CapMode[0] != m.CapMode && b.CapMode[0] != CapModeSphere {
+			xbeam.Attr = append(xbeam.Attr, xml.Attr{Name: xml.Name{Local: attrCap1}, Value: b.CapMode[0].String()})
+		}
+		if b.CapMode[1] != m.CapMode && b.CapMode[1] != CapModeSphere {
+			xbeam.Attr = append(xbeam.Attr, xml.Attr{Name: xml.Name{Local: attrCap2}, Value: b.CapMode[1].String()})
+		}
+		x.EncodeToken(xbeam)
+	}
+	x.SetAutoClose(false)
+	x.EncodeToken(xb.End())
 }
