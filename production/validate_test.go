93f6c9a416192bfc6610d59af5eb93fd260dac53
qmuntal <quimmuntal@gmail.com>
2020-03-10 19:06:51 +0100

    fix production tests

--- production/validate_test.go
+++ production/validate_test.go
@@ -2,6 +2,7 @@ package production
 
 import (
 	"encoding/xml"
+	"fmt"
 	"testing"
 
 	"github.com/go-test/deep"
@@ -24,11 +25,12 @@ func TestValidate(t *testing.T) {
 	}{
 		{"empty", args{new(go3mf.Model)}, []error{}},
 		{"buildEmptyUUID", args{&go3mf.Model{Namespaces: []xml.Name{{Space: ExtensionName}}, Build: go3mf.Build{
-			ExtensionAttr: go3mf.ExtensionAttr{mustUUID("")}}}}, []error{&specerr.BuildError{Err: specerr.ErrUUID}},
-		},
+			ExtensionAttr: go3mf.ExtensionAttr{mustUUID("")}}}}, []error{
+			fmt.Errorf("%s@Build: %v", rootPath, specerr.ErrUUID),
+		}},
 		{"buildNonValidUUID", args{&go3mf.Model{Namespaces: []xml.Name{{Space: ExtensionName}}, Build: go3mf.Build{
 			ExtensionAttr: go3mf.ExtensionAttr{mustUUID("a-b-c-d")}}}}, []error{
-			&specerr.BuildError{Err: specerr.ErrUUID},
+			fmt.Errorf("%s@Build: %v", rootPath, specerr.ErrUUID),
 		}},
 		{"extReq", args{&go3mf.Model{Namespaces: []xml.Name{{Space: ExtensionName}}, RequiredExtensions: []string{ExtensionName},
 			Childs: map[string]*go3mf.ChildModel{"/other.model": &go3mf.ChildModel{Resources: go3mf.Resources{Objects: []*go3mf.Object{validMesh}}}},
@@ -49,9 +51,9 @@ func TestValidate(t *testing.T) {
 			}},
 			Childs:    map[string]*go3mf.ChildModel{"/other.model": &go3mf.ChildModel{Resources: go3mf.Resources{Objects: []*go3mf.Object{validMesh}}}},
 			Resources: go3mf.Resources{Objects: []*go3mf.Object{&go3mf.Object{ID: 1, Mesh: validMesh.Mesh}}}}}, []error{
-			&specerr.ItemError{Index: 0, Err: specerr.ErrProdExtRequired},
-			&specerr.ItemError{Index: 2, Err: &specerr.MissingFieldError{Name: attrProdUUID}},
-			&specerr.ItemError{Index: 3, Err: specerr.ErrUUID},
+			fmt.Errorf("%s@Build@Item#0: %v", rootPath, specerr.ErrProdExtRequired),
+			fmt.Errorf("%s@Build@Item#2: %v", rootPath, &specerr.MissingFieldError{Name: attrProdUUID}),
+			fmt.Errorf("%s@Build@Item#3: %v", rootPath, specerr.ErrUUID),
 		}},
 		{"components", args{&go3mf.Model{Namespaces: []xml.Name{{Space: ExtensionName}}, Resources: go3mf.Resources{
 			Objects: []*go3mf.Object{
@@ -62,9 +64,9 @@ func TestValidate(t *testing.T) {
 				}},
 			},
 		}, Build: go3mf.Build{ExtensionAttr: go3mf.ExtensionAttr{mustUUID("f47ac10b-58cc-0372-8567-0e02b2c3d479")}}}}, []error{
-			&specerr.ObjectError{Path: rootPath, Index: 0, Err: specerr.ErrUUID},
-			&specerr.ObjectError{Path: rootPath, Index: 1, Err: &specerr.IndexedError{Name: "component", Index: 0, Err: &specerr.MissingFieldError{Name: attrProdUUID}}},
-			&specerr.ObjectError{Path: rootPath, Index: 1, Err: &specerr.IndexedError{Name: "component", Index: 1, Err: specerr.ErrUUID}},
+			fmt.Errorf("%s@Resources@Object#0: %v", rootPath, specerr.ErrUUID),
+			fmt.Errorf("%s@Resources@Object#1@Component#0: %v", rootPath, &specerr.MissingFieldError{Name: attrProdUUID}),
+			fmt.Errorf("%s@Resources@Object#1@Component#1: %v", rootPath, specerr.ErrUUID),
 		}},
 		{"child", args{&go3mf.Model{Namespaces: []xml.Name{{Space: ExtensionName}},
 			Build: go3mf.Build{ExtensionAttr: go3mf.ExtensionAttr{mustUUID("f47ac10b-58cc-0372-8567-0e02b2c3d479")}},
@@ -75,8 +77,8 @@ func TestValidate(t *testing.T) {
 						{ObjectID: 1, ExtensionAttr: go3mf.ExtensionAttr{&PathUUID{Path: "/b.model"}}},
 					}},
 				}}}}}}, []error{
-			&specerr.ObjectError{Path: "/other.model", Index: 0, Err: &specerr.IndexedError{Name: "component", Index: 0, Err: &specerr.MissingFieldError{Name: attrProdUUID}}},
-			&specerr.ObjectError{Path: "/other.model", Index: 0, Err: &specerr.IndexedError{Name: "component", Index: 0, Err: specerr.ErrProdRefInNonRoot}},
+			fmt.Errorf("/other.model@Resources@Object#0@Component#0: %v", &specerr.MissingFieldError{Name: attrProdUUID}),
+			fmt.Errorf("/other.model@Resources@Object#0@Component#0: %v", specerr.ErrProdRefInNonRoot),
 		}},
 	}
 	for _, tt := range tests {
