d728cc959a057387cc8135e2200c0efd17a5f8ea
qmuntal <quimmuntal@gmail.com>
2020-04-14 16:14:38 +0200

    reduce bench allocs

--- bench_test.go
+++ bench_test.go
@@ -2,11 +2,12 @@ package go3mf
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 )
 
 func BenchmarkUnmarshalModel(b *testing.B) {
-	bt := []byte(benchModel(1000))
+	bt := []byte(benchModel(10000))
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		m := new(Model)
@@ -34,16 +35,18 @@ func BenchmarkModel_Validate(b *testing.B) {
 }
 
 func benchModel(n int) string {
-	vertex := `<vertex x="100.000" y="100.000" z="100.000"/>`
-	triangle := `<triangle v1="0" v2="1" v3="2" pid="1" p1="1" p2="1" p3="1"/>`
-	v, t := vertex, triangle
+	vertex := []byte(`<vertex x="100.000" y="100.000" z="100.000"/>`)
+	triangle := []byte(`<triangle v1="0" v2="1" v3="2" pid="1" p1="1" p2="1" p3="1"/>`)
+	v, t := strings.Builder{}, strings.Builder{}
+	v.Grow(len(vertex) * n)
+	t.Grow(len(triangle) * n * 2)
 	for i := 0; i < n; i++ {
-		v += vertex
+		v.Write(vertex)
 	}
 	for i := 0; i < n*2; i++ {
-		t += triangle
+		t.Write(triangle)
 	}
-	return fmt.Sprintf(cubeModel, v, t)
+	return fmt.Sprintf(cubeModel, v.String(), t.String())
 }
 
 const cubeModel = `
